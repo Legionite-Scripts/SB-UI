0f8408a5d7c9ad133852852fa53ae0e903ef746b
legionite-scripts <samuelonwuka88@gmail.com>
2024-06-29 16:18:22 +0100

    Initial Commitd

---
+++ src/components/BtnComponent.vue
@@ -0,0 +1,32 @@
+<!-- src/components/Button.vue -->
+<template>
+  <button @click="onClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #42b983;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
